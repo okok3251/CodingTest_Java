78be930a9b96b321234e279e557e4bbf67403594
okok3251 <teemskorea1999@gmail.com>
2025-03-19 15:59:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = n+1;
-        while(BinCount(n) != BinCount(answer)){
+        while(BitCount(n) != BitCount(answer)){
             answer++;
         }
         return answer;
     }
     
-    public static int BinCount(int n){
+    public static int BitCount(int n){
         int count = 0;
         while(n >=1){
             if((n&1)==1) count++;
