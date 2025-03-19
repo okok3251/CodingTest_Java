168a7b3316b2fdac17a9862f339f83306a053d0e
okok3251 <teemskorea1999@gmail.com>
2025-03-19 15:32:47 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int count = 0;
+        while(n > 0){
+            if((n & 1) ==0){
+                n /= 2;
+            }else{
+                count++;
+                n -= 1;
+            }
+        }
+        return count;
+    }
+}
