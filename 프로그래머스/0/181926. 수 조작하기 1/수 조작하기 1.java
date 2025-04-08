71e15e9a94def23a1adbaecc041d488b09657aa3
okok3251 <teemskorea1999@gmail.com>
2025-04-08 15:36:44 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.58 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, String control) {
+        for(int i=0; i<control.length(); i++){
+            char c = control.charAt(i);
+            if(c == 'w'){
+                n++;
+            }
+            else if(c == 's'){
+                n--;
+            }
+            else if(c == 'd'){
+                n += 10;
+            }else{
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
