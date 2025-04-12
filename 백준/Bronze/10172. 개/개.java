273bdaf94c05d69b5bd5e85cc7d2f74bcd21d78f
okok3251 <teemskorea1999@gmail.com>
2025-04-12 14:16:54 +0900

    [Bronze V] Title: 개, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        long a = scan.nextLong();
+//        long b = scan.nextLong();
+//        long c = scan.nextLong();
+//        Solution10172.solution();
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
