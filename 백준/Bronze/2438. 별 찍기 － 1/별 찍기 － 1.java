c5089c84f69d63c8523cfc664c49c81eb03afb52
okok3251 <teemskorea1999@gmail.com>
2025-04-12 16:47:09 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+//        Solution2438.solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=1; i<=T; i++){
+            for(int j=0; j<i; j++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
