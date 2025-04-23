75559cf6bf9fe63304da55aaaa768c62eaeb50d5
okok3251 <teemskorea1999@gmail.com>
2025-04-23 14:27:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Solution2743.solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine().length()+ "");
+        bw.flush();
+        bw.close();
+    }
+}
