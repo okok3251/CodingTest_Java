8c8908b09157b64b517762db1d5d027d27a8a693
okok3251 <teemskorea1999@gmail.com>
2025-04-26 14:26:38 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Solution25083.solution();
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+}
